c5928e0eb956386ecc03dbc150c4722602bad33a
DKurilich <kurilicd@oregonstate.edu>
2023-11-07 18:44:27 -0800

    Dean DML Updates 11/7/23

--- DML.sql
+++ DML.sql
@@ -20,6 +20,9 @@ UPDATE Pilots SET name = :nameInput, birthdate= :birthdateInput, species = :spec
 DELETE FROM Pilots WHERE pilot_id = :pilot_ID_selected_from_browse_pilot_page;
 
 
+
+
+
 --
 --
 --starships.html queries
@@ -36,16 +39,8 @@ SELECT pilot_id, name FROM Pilots;
 --insert data for a new Starship into the Starships table
 INSERT INTO Starships (name, type, pilot_id) VALUES (:nameInput, :typeInput, :pilot_id_from_dropdown_Input);
 
---update a Starship
--- get all pilot_ids and names to populate the pilot dropdown
-SELECT pilot_id, name FROM Pilots;
---get a single Starship's information for the update Starship form
-SELECT starship_id, name, type, pilot_id FROM Starships WHERE starship_id = :starship_ID_selected_from_browse_starship_page;
---update a Starship's information based on submission of the Update Starship form
-UPDATE starship_id SET name = :nameInput, type = :typeInput, pilot_id = :pilot_id_from_dropdown_Input;
 
---delete a single Starships's information for the delete Pilot form
-DELETE FROM Starships WHERE starship_id = :starship_ID_selected_from_browse_starship_page;
+
 
 
 --
@@ -64,14 +59,7 @@ SELECT starship_id, name FROM Starships;
 --insert data for a new Weapon_Loadout into the Weapon_Loadouts table
 INSERT INTO Weapon_Loadouts (type, starship_id) VALUES (:typeInput, :starship_id_from_dropdown_Input);
 
---update a weapon loadout
---get a single weapon loadout's information for the update weapon loadouts form
-SELECT weapon_id, type, starship_id FROM Weapon_Loadouts WHERE weapon_id = :weapon_ID_selected_from_browse_weapon_loadout_page;
---update a Starship's information based on submission of the Update Starship form
-UPDATE weapon_id SET type = :typeInput, starship_id = :starship_id_from_dropdown_Input;
 
---delete a single Starships's information for the delete Pilot form
-DELETE FROM Weapon_Loadouts WHERE weapon_id = :weapon_ID_selected_from_browse_weapon_loadout_page;
 
 
 
@@ -87,15 +75,9 @@ SELECT * FROM Hyperspace_Routes;
 --add new Hyperspace Route
 --insert data for a new Weapon_Loadout into the Weapon_Loadouts table
 INSERT INTO Hyperspace_Routes (name) VALUES (:nameInput);
+INSERT INTO Planets_In_Routes (planet_id, route_id) VALUES (:planet_ID_Input, :hyperspace_route_ID_input);
 
---update a Hyperspace Route
---get a single Hyperspace Route's information for the update weapon loadouts form
-SELECT route_id, name FROM Hyperspace_Routes WHERE route_id = :route_ID_selected_from_browse_hyperspace_routes_page;
---update a Starship's information based on submission of the Update Starship form
-UPDATE route_id SET name = :nameInput;
 
---delete a single Starships's information for the delete Pilot form
-DELETE FROM Hyperspace_Routes WHERE route_id = :route_ID_selected_from_browse_Hyperspace_Routes_page;
 
 
 
@@ -111,15 +93,9 @@ SELECT * FROM Planets;
 --add new Planet
 --insert data for a new Planet
 INSERT INTO Planets (name, region) VALUES (:nameInput, :regionInput);
+INSERT INTO Planets_In_Routes (planet_id, route_id) VALUES (:planet_ID_Input, :hyperspace_route_ID_input);
 
---update a Planet
---get a single Hyperspace Route's information for the update weapon loadouts form
-SELECT planet_id, name, region FROM Planets WHERE planet_id = :planet_ID_selected_from_browse_Planets_page;
---update a Planet's information based on submission of the Update Planet form
-UPDATE planet_id, name, region SET name = :nameInput, region = :regionInput;
 
---delete a single Planet's information for the delete Pilot form
-DELETE FROM Planets WHERE planet_id = :planet_ID_selected_from_browse_Planets_page;
 
 
 --
@@ -153,6 +129,8 @@ DELETE FROM Starship_Route_Permits WHERE permit_id = :permit_ID_selected_from_br
 
 
 
+
+
 --
 --
 --planets_in_routes.html queries
@@ -170,4 +148,4 @@ SELECT planet_id, name FROM Planets;
 -- get all route_ids and names to populate the Hyperspace Routes dropdown
 SELECT route_id, name FROM Hyperspace_Routes;
 --insert data for a new Starship Route Permit
-INSERT INTO Planets_In_Routes (planet_id, route_id) VALUES (:planet_ID_Input, hyperspace_route_ID_input, :dateInput) WHERE :planet_ID_Input=:planet_id_from_dropdown_Input, hyperspace_route_ID_input = :hyperspace_route_ID_from_dropdown_Input;
+INSERT INTO Planets_In_Routes (planet_id, route_id) VALUES (:planet_id_from_dropdown_Input, :hyperspace_route_ID_from_dropdown_Input, :dateInput);
